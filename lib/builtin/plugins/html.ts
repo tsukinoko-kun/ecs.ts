edf8cba411f507246452036d3fb1c68a4112dde0
Frank Mayer <gh@frankmayer.dev>
2024-07-24 18:31:43 +0200

    feat(html): html root can now be specified as Element reference

--- lib/builtin/plugins/html.ts
+++ lib/builtin/plugins/html.ts
@@ -4,9 +4,11 @@ import { Schedule } from "../../schedule"
 import { cleanupHtmlInteraction, htmlInteraction, renderHtmlRoot } from "../systems"
 import { Commands } from "../../commands"
 
-export function HtmlPlugin(rootSelector: string): Plugin {
+export function HtmlPlugin(rootSelector: string): Plugin
+export function HtmlPlugin(rootElement: Element): Plugin
+export function HtmlPlugin(root: string | Element): Plugin {
     return (app) => {
-        Commands.insertResource(new HtmlRoot(rootSelector))
+        Commands.insertResource(new HtmlRoot(root))
         app.addSystem(Schedule.Update, renderHtmlRoot)
         app.addSystem(Schedule.Startup, htmlInteraction)
         app.addSystem(Schedule.Last, cleanupHtmlInteraction)
--- lib/builtin/resources/htmlRoot.ts
+++ lib/builtin/resources/htmlRoot.ts
@@ -1,12 +1,18 @@
 export class HtmlRoot {
     public readonly element: Element
 
-    public constructor(selector: string) {
-        const root = document.querySelector(selector)
-        if (root == null) {
-            throw new Error(`Root element with selector ${selector} not found in the document`)
+    public constructor(rootSelector: string)
+    public constructor(rootElement: Element)
+    public constructor(root: string | Element)
+    public constructor(root: string | Element) {
+        if (typeof root === "string") {
+            const el = document.querySelector(root)
+            if (el == null) {
+                throw new Error(`Root element with selector ${root} not found in the document`)
+            }
+            this.element = el
+        } else {
+            this.element = root
         }
-
-        this.element = root
     }
 }
